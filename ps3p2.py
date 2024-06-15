8102f3982afe9dc12031d163072db114eb1cf54c
CIS106-MichaelSwan <mwswan99@gmail.com>
2024-06-14 23:17:59 -0500

    Add files via upload

---
+++ ps3p2.py
@@ -0,0 +1,11 @@
+#input phase
+lname = input("Enter your last name: ")
+midterm = float(input("Enter your midterm score: "))
+final = float(input("Enter your final score: "))
+
+#process phase
+total = midterm + final
+
+#output phase
+print("Student: ", lname)
+print("Total points earned: ", total)
