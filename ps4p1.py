eb1d3f610a76e8c1ea54b86cadc766a87dbb033b
CIS106-MichaelSwan <mwswan99@gmail.com>
2024-06-23 23:29:02 -0500

    Add files via upload

---
+++ ps4p1.py
@@ -0,0 +1,10 @@
+#input phase
+exam1 = float(input("Enter score 1: "))
+exam2 = float(input("Enter score 2: "))
+
+
+#process phase
+total = (exam1 * 0.60) + (exam2 * 0.40)
+
+#output phase
+print("Total score is: ", total)
